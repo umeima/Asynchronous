5154c54a9475a3c9deab81429068343e5cfd3849
oumaima <oumaima.belmouadden2@gmail.com>
2020-04-17 11:50:06 +0100

    défi2 version final

--- Défi_2/app.js
+++ Défi_2/app.js
@@ -12,7 +12,8 @@ function loadCustomer (){
     request1.onload = function (){       // fonction pour analyser le process de la requete envoyé
         if (request1.readyState == 4){
             if (request1.status == 200) {
-                document.getElementById('customer').innerHTML=request1.response
+                var cust = JSON.parse(request1.response)
+                document.getElementById('customer').innerHTML= "name: " +  cust.name + "<br>" + "company: " +  cust.company + "<br>" + "phone: " +  cust.phone +"<br>" + "id: " +  cust.id 
                 
             }
             if (request1.status == 404) {
@@ -31,11 +32,17 @@ function loadCustomers (){
     var request2 = new XMLHttpRequest;
 
     request2.onload = function (){       // fonction pour analyser le process de la requete envoyé
-        if (request2.readyState == 4){
+        
             if (request2.status == 200) {
-                document.getElementById('customers').innerHTML=request2.response
+                var custs = JSON.parse(request2.response)
+                var data='';
+                for( var i in custs ){
+                    data +="name: " +  custs[i].name + "<br>" + "company: " + custs[i].company + "<br>" + "phone: " + custs[i].phone +"<br>" + "id: " + custs[i].id + "<hr>";
                 
-            }
+                 
+        document.getElementById('customers').innerHTML = data
+    }
+    
             if (request2.status == 404) {
                 console.log("ERROR 404")
             }
